37eb9f6ba15cd68411722b00dd04250d1ed99ed1
Ajit <ajitsharma.bhardhwaj@gmail.com>
2026-08-26 16:52:46 +0530

    Python Assignment-1 Uploding

---
+++ Python Assignments/Assignment-1/Variable.py
@@ -0,0 +1,95 @@
+# Question-18
+x = 25
+print(type(x))
+
+# Question-19
+price = 99.50
+print(type(price))
+
+# Question-20
+name = "Python"
+print(type(name))
+
+# Question-21
+is_active = True
+print(type(is_active))
+
+# Question-22
+data = None
+print(type(data))
+
+# Question-23
+y=25
+price=69.69
+name='Ram'
+is_present=True
+Name=None
+print(type(y), type(price), type(name), type(is_present), type(Name))
+
+# Question-24
+Student_name='Lakshya'
+Student_age=19
+Student_height=5.11
+Whether_the_person_is_student=True
+Student_result_where_no_result_is_currently_available=None
+
+
+Student_age=19
+print(type(Student_name))
+print(type(Student_age))
+print(type(Student_height))
+print(type(Whether_the_person_is_student))
+print(type(Student_result_where_no_result_is_currently_available))
+
+# Question-25
+a = 50
+b = 50.0
+c = "50"
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+# Question-26
+a = True
+b = "True"
+print(type(a))
+print(type(b))
+
+# Question-27
+a = None
+b = "None"
+print(type(a))
+print(type(b))
+
+# Question-28
+a=21
+print(type(a))
+a='lakshya'
+print(type(a))
+# int changed to str
+
+# Question-29
+Product_name='Fashwash'
+Product_quantity=2
+Product_price=99.65
+Product_value=True
+Product_discount=None
+print(type(Product_name))
+print(type(Product_quantity))
+print(type(Product_price))
+print(type(Product_value))
+print(type(Product_discount))
+
+# Question-30
+print(type(10))
+print(type(10.0))
+print(type("10"))
+print(type(True))
+print(type('True'))
+print(type(None))
+print(type("None"))
+
+
+
+
