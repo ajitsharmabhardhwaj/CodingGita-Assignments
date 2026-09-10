b8c1e59eb6b86bb0693f0860bfb7894e9c471347
Ajit <ajitsharma.bhardhwaj@gmail.com>
2026-09-10 17:35:19 +0530

    Create 5.String Indexing.py

---
+++ Python Assignments/Assignment-3/5.String Indexing.py
@@ -0,0 +1,42 @@
+# Answer-19
+text = "PYTHON"
+
+print(text[0])
+print(text[1])
+print(text[-1])
+print(text[-2])
+
+
+# Answer-20
+text = "COMPUTER"
+
+print(text[0])
+print(text[3])
+print(text[-1])
+print(text[-3])
+
+
+# Answer-21
+text = "PYTHON"
+
+print(text[0])
+print(text[2])
+print(text[-1])
+print(text[-2])
+
+
+# Answer-22
+word = input("Enter a word: ")
+
+print("First character:", word[0])
+print("Last character:", word[-1])
+
+
+# Answer-23
+word = "PROGRAM"
+
+print(word[0])
+print(word[2])
+print(word[-1])
+print(word[-4])
+
