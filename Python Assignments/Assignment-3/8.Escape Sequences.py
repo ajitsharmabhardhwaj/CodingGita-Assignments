1911859cdfff8c06f4b8f1bf02a46768cb26d053
Ajit <ajitsharma.bhardhwaj@gmail.com>
2026-09-15 09:39:31 +0530

    Add escape sequences examples in Python

---
+++ Python Assignments/Assignment-3/8.Escape Sequences.py
@@ -0,0 +1,31 @@
+# Answer-42
+print("Hello\nWorld")
+
+
+# Answer-43
+print("Name:\tRahul")
+print("Age:\t20")
+print("City:\tAhmedabad")
+
+
+# Answer-44
+print("C:\\Python\\Programs")
+
+
+# Answer-45
+print("It\'s Python")
+
+
+# Answer-46
+print("He said \"Hello\"")
+
+
+# Answer-47
+print("Python\nProgramming")
+
+
+# Answer-48
+print("Student Details\n")
+print("Name:\tRahul")
+print("Age:\t20")
+print("Course:\tB.Tech")
