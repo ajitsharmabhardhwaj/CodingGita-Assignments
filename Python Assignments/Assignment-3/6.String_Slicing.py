543a122c322f4df52b48a8970045d3dfe208591f
Ajit <ajitsharma.bhardhwaj@gmail.com>
2026-09-15 09:36:58 +0530

    Create 6.String_Slicing.py

---
+++ Python Assignments/Assignment-3/6.String_Slicing.py
@@ -0,0 +1,70 @@
+# Answer-24
+text = "PYTHON"
+
+print(text[0:3])
+print(text[2:5])
+print(text[1:6])
+
+
+# Answer-25
+text = "PROGRAMMING"
+
+print(text[:4])
+print(text[4:])
+print(text[:])
+
+
+# Answer-26
+text = "COMPUTER"
+
+print(text[-5:])
+print(text[:-3])
+print(text[-6:-2])
+
+
+# Answer-27
+text = "PYTHON"
+
+print(text[::2])
+print(text[1::2])
+print(text[::-1])
+
+
+# Answer-28
+text = input("Enter a string: ")
+
+print(text[::-1])
+
+
+# Answer-29
+text = input("Enter a string: ")
+
+print(text[::2])
+
+
+# Answer-30
+text = input("Enter a string: ")
+
+print("First three characters:", text[:3])
+print("Last three characters:", text[-3:])
+
+
+# Answer-31
+text = "ABCDEFGHIJ"
+
+print(text[2:8:2])
+print(text[8:2:-2])
+print(text[::-2])
+
+# text[2:8:2]  -> start=2, stop=8, step=2
+# text[8:2:-2] -> start=8, stop=2, step=-2
+# text[::-2]   -> start=None, stop=None, step=-2
+
+
+# Answer-32
+text = "BTECH-CSE-2026"
+
+print(text[:5])
+print(text[6:9])
+print(text[10:])
+
