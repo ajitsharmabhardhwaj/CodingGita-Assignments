0cbce670919d248541f87b06245cb442e938438c
Ajit <ajitsharma.bhardhwaj@gmail.com>
2026-09-15 09:31:19 +0530

    Create 1.Comperison_Operators.py

---
+++ Python Assignments/Assignment-3/1.Comperison_Operators.py
@@ -0,0 +1,38 @@
+# Answer-1
+a = 15
+b = 20
+
+print(a < b)
+print(a > b)
+print(a == b)
+print(a != b)
+print(a <= b)
+print(a >= b)
+
+
+# Answer-2
+x = 10
+y = 10
+
+print(x == y)
+print(x != y)
+print(x < y)
+print(x <= y)
+print(x >= y)
+
+
+# Answer-3
+a = 10
+b = 5
+
+print(a + b == 15)
+print(a * b > 40)
+print(a - b != 5)
+print(a // b == 2)
+
+
+# Answer-4
+print("Python" == "Python")
+print("Python" == "python")
+print("Hello" != "hello")
+
